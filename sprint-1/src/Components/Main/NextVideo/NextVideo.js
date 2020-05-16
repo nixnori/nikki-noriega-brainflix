048fb194213104abdeb5474c5d1fe1461647f704
Nikki <aletheanicole.n@gmail.com>
2020-05-16 02:22:29 -0400

    Style Next Video section

--- sprint-1/src/Components/Main/NextVideo/NextVideo.js
+++ sprint-1/src/Components/Main/NextVideo/NextVideo.js
@@ -4,21 +4,22 @@ import './NextVideo.scss';
 const NextVideo = props => {
     return (
         <div className="video__section">
-        {
-            props.videoList.map(video => {
-                if (props.state !== video) {
-                    return (
-                        <div className="video__container" key={video.id}>
-                            <img className="video__image" src={video.image} />
-                            <div className="video__text">
-                                <h4 className="video__title">{video.title}</h4>
-                                <p className="video__channel">{video.channel}</p>
+            <h4 className="video__header">NEXT VIDEO</h4>
+            {
+                props.videoList.map(video => {
+                    if (props.state !== video) {
+                        return (
+                            <div className="video__container" key={video.id}>
+                                <img className="video__image" src={video.image} />
+                                <div className="video__text">
+                                    <h4 className="video__title">{video.title}</h4>
+                                    <p className="video__channel">{video.channel}</p>
+                                </div>
                             </div>
-                        </div>
-                    )
-                }
-            })
-        }
+                        )
+                    }
+                })
+            }
         </div>
     )
 }
