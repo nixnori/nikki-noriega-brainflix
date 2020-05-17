1d1c337746a35e351f38e18293a15f358f97eb66
Nikki <aletheanicole.n@gmail.com>
2020-05-17 16:01:25 -0400

    Finish styling video

--- sprint-1/src/Components/Main/Video/Video.js
+++ sprint-1/src/Components/Main/Video/Video.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import vid0 from '../../../assets/Images/video-list-0.jpg';
 import './Video.scss';
+import play from '../../../assets/Icons/SVG/Icon-play.svg';
+import fullscreen from '../../../assets/Icons/SVG/Icon-fullscreen.svg';
+import volume from '../../../assets/Icons/SVG/Icon-volume.svg';
 
 
 
 const Video = () => {
     return (
         <div className="main-video__container">
-            <video className="main-video" poster={vid0} controls></video>
+            <video className="main-video" poster={vid0}>
+            </video>
+            <div className="main-video__controls">
+                <div className="main-video__play"><img src={play} /></div>
+                <div className="main-video__scrubber"><hr className="main-video__scrubber--line"></hr><span className="main-video__time">0:000:42</span></div>
+                <div className="main-video__full-vol"><img src={fullscreen} /><img src={volume} /></div>
+            </div>
         </div>
     )
 }
